59fbb9528388724a67c81b53e7b9ab6c9eec8461
Jemari Lapera <jealapera@gmail.com>
2024-02-22 17:36:54 +0800

    task/TRAIN-1095 Fix: Fixed storybook for Checkbox

--- src/components/Elements/Checkbox/Checkbox.stories.tsx
+++ src/components/Elements/Checkbox/Checkbox.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StoryObj, Meta } from '@storybook/react';
 import Checkbox, { CheckboxProps } from './Checkbox';
 
@@ -6,6 +7,7 @@ const meta: Meta<CheckboxProps> = {
   component: Checkbox,
   argTypes: {
     onChange: { action: 'changed' },
+    onClick: { action: 'clicked' },
   },
 };
 
@@ -13,9 +15,27 @@ export default meta;
 
 type StoryType = StoryObj<CheckboxProps>;
 
-export const Default: StoryType = {
-  args: {
-    checked: false,
-    onChange: () => {},
-  },
+export const Default = (args: StoryType) => {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleOnChange = () => {
+    setChecked(!checked);
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    console.log('Clicked');
+  };
+
+  return (
+    <Checkbox
+      {...args}
+      id="checkbox"
+      checked={checked}
+      onChange={handleOnChange}
+      onClick={handleClick}
+    />
+  );
 };
+
+Default.args = {};
